ad3e26b755a31048f7ae3f6e93ef03881a8f7a4f
mmm1379 <mmmasoudpour@gmail.com>
2020-04-10 16:29:20 +0430

    optimized view and controller

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+import View.MainPageView;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MainPageView mainPageView =new MainPageView();
+        mainPageView.run();
+    }
+}
--- src/main/java/Models/SellLog.java
+++ src/main/java/Models/SellLog.java
@@ -1,14 +1,16 @@
 package Models;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 public class SellLog {
     private String code;
     private Date date;
     private int money;
-    private int offAmount;
+    private int offAmount; //if existed
+    private ArrayList<Product> soldItems;
     private String buyerName;
-    private String sellerName;
     private String deliverySituation;
+
 
 }
