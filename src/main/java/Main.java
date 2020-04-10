2cb5032a2f95b2cc6145f24f6b5d5f7705576d51
bpc <matinmoradi110@gmail.com>
2020-04-10 17:24:28 +0430

    yes print test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,11 @@ import View.MainPageView;
 
 public class Main {
 
+
     public static void main(String[] args) {
         MainPageView mainPageView =new MainPageView();
         mainPageView.run();
+
+        System.out.print("yes");
     }
 }
