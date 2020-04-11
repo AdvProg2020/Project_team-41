d6f44ffc9768f1d6f96e036b968bfe82a6c512e7
mmm1379 <mmmasoudpour@gmail.com>
2020-04-11 19:23:19 +0430

    made abstract menu

--- src/main/java/View/Menu.java
+++ src/main/java/View/Menu.java
@@ -1,15 +1,53 @@
 package View;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public abstract class Menu {
     private Menu superMenu;
     private ArrayList<Menu> subMenus;
     private String name;
-
+    private static Scanner scanner = new Scanner(System.in);
     public Menu(Menu superMenu, String name) {
         this.superMenu = superMenu;
         this.name = name;
+    }
+    private void show(){
+        for (Menu subMenu : subMenus) {
+            System.out.println(subMenu.getName());
+        }
+        if(superMenu != null)
+            System.out.println("back");
+        this.execute();
+    }
+    private void execute(){
+        String command = scanner.nextLine();
+        if(command.equals("help"))
+            this.show();
+        if(command.equals("back")) {
+            if (superMenu != null) {
+                superMenu.show();
+                superMenu.execute();
+            }
+            else{
+                //todo print invalid command
+            }
+
+        }
+
+
+        for (Menu subMenu : subMenus) {
+            if(command.equals(subMenu.getName())) {
+                subMenu.show();
+                subMenu.execute();
+
+            }
+        }
+    }
+
+
+    public String getName() {
+        return name;
     }
 
 }
