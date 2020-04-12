b7b68a6f9cc50bb32f14f34f048a39975464cf43
mahdimostajer <mahdi.mostajeran.ce@gmail.com>
2020-04-12 20:34:49 +0430

    login

--- src/main/java/View/Menus/RegisterLogin.java
+++ src/main/java/View/Menus/RegisterLogin.java
@@ -2,7 +2,33 @@ package View.Menus;
 
 public class RegisterLogin extends Menu {
     public RegisterLogin(Menu superMenu) {
-        super(superMenu, "RegisterLogin");
+        //toDoIfLogged
+            super(superMenu, "RegisterLogin");
+            this.addSubMenu(new Menu(this, "Create") {
+                @Override
+                public void show() {
+                    //to do
+                }
+
+                @Override
+                public void execute() {
+                    //to do
+                }
+            });
+            this.addSubMenu(new Menu(this, "login") {
+                @Override
+                public void show() {
+                    //to do
+                }
+
+                @Override
+                public void execute() {
+                    //to do
+                }
+            });
+
+        //elseLogout
+
 
     }
 }
