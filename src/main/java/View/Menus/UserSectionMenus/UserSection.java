c4fd7f820d4785021152ffbd2a6e8de05620eba6
mmm1379 <mmmasoudpour@gmail.com>
2020-04-13 20:31:14 +0430

    coded a bit of Manager

--- src/main/java/View/Menus/UserSectionMenus/UserSection.java
+++ src/main/java/View/Menus/UserSectionMenus/UserSection.java
@@ -1,5 +1,19 @@
 package View.Menus.UserSectionMenus;
 
-public abstract class UserSection {
+import Controller.UserSectionController;
+import Models.Person.ViewPersonalInfo;
+import View.Menus.Menu;
+
+import java.util.ArrayList;
+
+public abstract class UserSection extends Menu {
+
+    public UserSection(Menu superMenu, String name) {
+        super(superMenu, name);
+        addSubMenu(new ViewPersonalInfo(this));
+    }
+
+
+
 
 }
