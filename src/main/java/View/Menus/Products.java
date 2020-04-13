75dee610240f565748af95008aba45e1c1bc3e37
mahdimostajer <mahdi.mostajeran.ce@gmail.com>
2020-04-13 15:48:50 +0430

    sortAndFilter

--- src/main/java/View/Menus/Products.java
+++ src/main/java/View/Menus/Products.java
@@ -4,7 +4,10 @@ public class Products extends Menu {
     public Products(Menu superMenu) {
         super(superMenu, "Products");
         addSubMenu(new Product(this));
+        addSubMenu(new Filter(this));
+        addSubMenu(new Sort(this));
     }
 
     //todo getter for unnamed subclasses
+    //todo show and execute override
 }
