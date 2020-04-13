78538f3f135d9e2f3fc351aff40e55efff6195c9
mahdimostajer <mahdi.mostajeran.ce@gmail.com>
2020-04-13 16:23:07 +0430

    products

--- src/main/java/View/Menus/Product.java
+++ src/main/java/View/Menus/Product.java
@@ -1,8 +1,40 @@
 package View.Menus;
 
 public class Product extends Menu {
+    private int id;
     public Product(Menu superMenu) {
         super(superMenu, "Product");
+        addSubMenu(addDigest());
     }
 
+    @Override
+    public void show() {
+        super.show();
+    }
+
+    @Override
+    public void execute() {
+        super.execute();
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public Menu addDigest(){
+        return new Menu(this,"Digest") {
+            @Override
+            public void show() {
+                super.show();
+            }
+
+            @Override
+            public void execute() {
+                super.execute();
+            }
+        };
+    }
 }
