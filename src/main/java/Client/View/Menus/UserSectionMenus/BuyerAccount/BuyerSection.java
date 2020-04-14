b472da7031809a4b37d1a7902669c34c89ca4947
bpc <matinmoradi110@gmail.com>
2020-04-14 18:28:25 +0430

    BuyerSection_general fields & methods was written

--- src/main/java/Client/View/Menus/UserSectionMenus/BuyerAccount/BuyerSection.java
+++ src/main/java/Client/View/Menus/UserSectionMenus/BuyerAccount/BuyerSection.java
@@ -1,5 +1,6 @@
 package Client.View.Menus.UserSectionMenus.BuyerAccount;
 
+import Client.Controller.BuyerAccountController.BuyerController;
 import Client.Models.Person.ViewPersonalInfo;
 import Client.View.Menus.Menu;
 import Client.View.Menus.UserSectionMenus.UserSection;
@@ -9,20 +10,66 @@ public class BuyerSection extends UserSection {
         super(superMenu, name);
         this.addSubMenu(new ViewPersonalInfo(this));
         this.addSubMenu(new ViewCart(this));
+        this.addSubMenu(new Purchase(this));
+        this.addSubMenu(viewOrders());
+        this.addSubMenu(viewBalance());
+        this.addSubMenu(viewDiscountCodes());
     }
 
-    private Menu viewCart(){
-            return new Menu(this , "view cart") {
-                @Override
-                public void show() {
+    private Menu viewOrders() {
+        return new Menu(this, "view order") {
+            @Override
+            public void show() {
+                //TODO show shopping history
+            }
 
+            @Override
+            public void execute() {
+                String input;
+                while (!(input = scanner.nextLine()).equals("back")) {
+                    if (input.startsWith("show order")) {
+                        String orderId = "";
+                        BuyerController.showTheOrder(orderId);
+                        //TODO print the details
+                    } else if (input.startsWith("rate")) {
+                        //TODO get productID & score
+                        String productId = "";
+                        int score = -1;
+                        BuyerController.rateTheProduct(productId, score);
+                        //TODO print the result
+                    }
+                    this.superMenu.show();
+                    this.superMenu.execute();
                 }
-
-                @Override
-                public void execute() {
-
-                }
-            };
+            }
+        };
     }
 
+    private Menu viewBalance(){
+        return new Menu(this , "view balance") {
+            @Override
+            public void show() {
+                //TODO print the balance
+            }
+
+            @Override
+            public void execute() {
+                super.execute();
+            }
+        };
+    }
+
+    private Menu viewDiscountCodes(){
+        return new Menu(this , "view discount codes") {
+            @Override
+            public void show() {
+                //TODO print discount codes
+            }
+
+            @Override
+            public void execute() {
+                super.execute();
+            }
+        };
+    }
 }
