0c89c7f8afa2dc7543f7ad82ca4d5539bb210c13
mmm1379 <mmmasoudpour@gmail.com>
2020-04-14 12:42:42 +0430

    made ManagerSection in view

--- src/main/java/Client/View/Menus/UserSectionMenus/ManagerSection.java
+++ src/main/java/Client/View/Menus/UserSectionMenus/ManagerSection.java
@@ -1,13 +1,22 @@
 package Client.View.Menus.UserSectionMenus;
 
+import Client.Controller.UserSectionController;
 import Client.Models.Person.Person;
 import Client.View.Menus.Menu;
+import Client.View.Menus.UserSectionMenus.UserSection;
+
+import java.util.ArrayList;
 
 public class ManagerSection extends UserSection {
 
     public ManagerSection(Menu superMenu) {
         super(superMenu, "ManagerSection");
         addSubMenu(this.addManageUsers());
+        addSubMenu(this.addCreateDiscountCode());
+        addSubMenu(this.addManageAllProducts());
+        addSubMenu(this.addManageCategories());
+        addSubMenu(this.addManageRequests());
+        addSubMenu(this.addViewDiscountCodes());
 
     }
 
@@ -22,21 +31,141 @@ public class ManagerSection extends UserSection {
         super.execute();
 
     }
+
+    private Menu addManageCategories(){
+        return new Menu(this,"ManagerCategories") {
+            private void edit(String Category){
+
+            }
+            private void add(String Category){
+
+            }
+            private void remove(String Category){
+
+            }
+
+
+            @Override
+            public void show() {
+                super.show();
+            }
+
+            @Override
+            public void execute() {
+                super.execute();
+            }
+        };
+    }
+
+    private Menu addManageRequests(){
+        return new Menu(this,"ManageRequests") {
+
+            private void details(String requestId){
+
+            }
+            private void accept(String requestId){
+
+            }
+            private void decline(String requestId){
+
+            }
+
+
+            @Override
+            public void show() {
+                super.show();
+            }
+
+            @Override
+            public void execute() {
+                super.execute();
+            }
+        };
+    }
+
+    private Menu addViewDiscountCodes(){
+        return new Menu(this,"ViewDiscountCodes") {
+            private void viewDiscountCode(String code){
+
+            }
+            private void editDiscountCode(String code){
+
+            }
+            private void removeDiscountCode(String code){
+
+            }
+
+            @Override
+            public void show() {
+                super.show();
+            }
+
+            @Override
+            public void execute() {
+                super.execute();
+            }
+        };
+    }
+
+    private Menu addCreateDiscountCode(){
+        return new Menu(this,"CreateDiscountCode") {
+            @Override
+            public void show() {
+                super.show();
+            }
+
+            @Override
+            public void execute() {
+                super.execute();
+            }
+        };
+    }
+
+    private Menu addManageAllProducts(){
+        return new Menu(this,"ManageAllProducts") {
+            private void remove(String productId){
+
+            }
+
+            @Override
+            public void show() {
+                super.show();
+            }
+
+            @Override
+            public void execute() {
+                super.execute();
+            }
+
+        };
+    }
+
     private Menu addManageUsers(){
         return new Menu(this,"manageUsers") {
-                    public void view(Person username){
 
-                    }
-                    @Override
-                    public void show() {
-                        super.show();
-                    }
+            private void view(String username){
 
-                    @Override
-                    public void execute() {
-                        super.execute();
-                    }
-                };
+            }
+            private void deleteUser(String username){
+
+            }
+            private void createManagerProfile(){
+
+            }
+
+
+            @Override
+            public void execute() {
+                super.execute();
+
+
+            }
+            @Override
+            public void show() {
+                super.show();
+            }
+
+        };
     }
 
 }
