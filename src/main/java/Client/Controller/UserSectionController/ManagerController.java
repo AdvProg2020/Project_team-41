a58d06456dc707865c6ec475a8a6e358830f61c8
mmm1379 <mmmasoudpour@gmail.com>
2020-04-14 13:43:34 +0430

    designed ManagerController

---
+++ src/main/java/Client/Controller/UserSectionController/ManagerController.java
@@ -0,0 +1,58 @@
+package Client.Controller.UserSectionController;
+
+import Client.Models.Person.Person;
+
+import java.util.ArrayList;
+
+public class ManagerController {
+    public static Person viewUser(String username){
+
+    }
+    public static boolean deleteUser(String username){
+        //returns boolean for error handling
+    }
+    public static String createManagerProfile(ArrayList<String> userInfo){
+        //returns String for error handling
+    }
+    public static boolean removeProduct(String productId){
+        //returns boolean for error handling
+    }
+    public static void createDiscountCode(ArrayList<String> codeInformation){
+
+    }
+    public static ArrayList<String> viewDiscountCode(String code){
+
+    }
+    public static String editDiscountCode(String command,String code){
+        //returns String for error handling
+    }
+    public static boolean removeDiscountCode(String code){
+
+    }
+    public static ArrayList<String> showRequest(){
+
+    }
+    public static ArrayList<String> getRequestDetails(String request){
+
+    }
+    public boolean acceptRequest(String request){
+
+    }
+    public boolean declineRequest(String request){
+
+    }
+    public ArrayList<String> showCategories(){
+
+    }
+    public boolean editCategory(String category,String command){
+
+    }
+    public boolean addCategory(String categoryName,ArrayList<String> categoryInformation){
+
+    }
+    public boolean removeCategory(String categoryName){
+
+    }
+
+
+}
