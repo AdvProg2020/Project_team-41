e8fe6e599f1e5d3be75473e1cdc94b59abc47102
mmm1379 <mmmasoudpour@gmail.com>
2020-04-14 19:46:12 +0430

    made controller for offs

--- src/main/java/Client/View/Menus/Offs.java
+++ src/main/java/Client/View/Menus/Offs.java
@@ -3,5 +3,16 @@ package Client.View.Menus;
 public class Offs extends Menu {
     public Offs(Menu superMenu) {
         super(superMenu, "Offs");
+        addSubMenu(new Product(this));
+    }
+
+    @Override
+    public void show() {
+        super.show();
+    }
+
+    @Override
+    public void execute() {
+        super.execute();
     }
 }
