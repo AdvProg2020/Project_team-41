78af86d4b8f1871ce18787ba5445ffab45f840af
mahdimostajer <mahdi.mostajeran.ce@gmail.com>
2020-04-14 19:13:16 +0430

    SellerSection

--- src/main/java/Client/View/Menus/UserSectionMenus/SellerSection.java
+++ src/main/java/Client/View/Menus/UserSectionMenus/SellerSection.java
@@ -1,4 +1,142 @@
 package Client.View.Menus.UserSectionMenus;
 
+import Client.View.Menus.Menu;
+
 public class SellerSection extends UserSection {
+
+    public SellerSection(Menu superMenu) {
+        super(superMenu, "SellerSection");
+        addSubMenu(addViewCompanyInfo());
+        addSubMenu(addViewSalesHistory());
+        addSubMenu(addMangeProduct());
+        addSubMenu(addAddProduct());
+        addSubMenu(addViewOffs());
+        addSubMenu(addShowCategories());
+        addSubMenu(addRemoveProduct());
+        addSubMenu(addViewBalance());
+    }
+
+    public Menu addViewCompanyInfo(){
+        return new Menu(this,"ViewCompanyInfo") {
+            @Override
+            public void show() {
+                //todo
+            }
+
+            @Override
+            public void execute() {
+                //todo
+            }
+        };
+    }
+    public Menu addViewSalesHistory(){
+        return new Menu(this,"ViewSalesHistory") {
+            @Override
+            public void show() {
+                //todo
+            }
+
+            @Override
+            public void execute() {
+                //todo
+            }
+        };
+    }
+    public Menu addMangeProduct(){
+        return new Menu(this,"MangeProduct") {
+            @Override
+            public void show() {
+                //todo
+            }
+
+            @Override
+            public void execute() {
+                //todo
+            }
+            public void viewProduct(int id){
+
+            }
+            public void viewProductBuyers(int id){
+
+            }
+            public void editProduct(int id){
+
+            }
+        };
+    }
+    public Menu addAddProduct(){
+        return new Menu(this,"AddProduct") {
+            @Override
+            public void show() {
+                //todo
+            }
+
+            @Override
+            public void execute() {
+                //todo
+            }
+        };
+    }
+    public Menu addViewOffs(){
+        return new Menu(this,"ViewOffs") {
+            @Override
+            public void show() {
+                //todo
+            }
+
+            @Override
+            public void execute() {
+                //todo
+            }
+            public void viewOff(int id){
+
+            }
+            public void editOff(int id){
+
+            }
+            public void addOff(){
+
+            }
+        };
+    }
+    public Menu addShowCategories(){
+        return new Menu(this,"ShowCategories") {
+            @Override
+            public void show() {
+                //todo
+            }
+
+            @Override
+            public void execute() {
+                //todo
+            }
+        };
+    }
+    public Menu addRemoveProduct(){
+        return new Menu(this,"RemoveProduct") {
+            @Override
+            public void show() {
+                //todo
+            }
+
+            @Override
+            public void execute() {
+                //todo
+            }
+        };
+    }
+    public Menu addViewBalance(){
+        return new Menu(this,"ViewBalance") {
+            @Override
+            public void show() {
+                //todo
+            }
+
+            @Override
+            public void execute() {
+                //todo
+            }
+        };
+    }
+
 }
