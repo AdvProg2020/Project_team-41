fb7561ec4411d10f706d2008f02931f5fb96b72e
mahdimostajer <mahdi.mostajeran.ce@gmail.com>
2020-04-15 17:18:11 +0430

    FilterController

--- src/main/java/Client/Controller/FilterController.java
+++ src/main/java/Client/Controller/FilterController.java
@@ -1,5 +1,11 @@
 package Client.Controller;
 
+import Client.Models.Category;
+import Client.Models.Product;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class FilterController {
     private static FilterController single_instance = null;
     public static FilterController getInstance()
@@ -9,6 +15,29 @@ public class FilterController {
 
         return single_instance;
     }
+
+    private Category filterCategory;
+    private HashMap<String ,String> filterFeature;
+
     private FilterController(){
+    }
+    public void setFilterCategory(Category filterCategory) {
+        this.filterCategory = filterCategory;
+    }
+
+    public void setFilterFeature(HashMap<String, String> filterFeature) {
+        this.filterFeature = filterFeature;
+    }
+
+    public Category getFilterCategory() {
+        return filterCategory;
+    }
+
+    public HashMap<String, String> getFilterFeature() {
+        return filterFeature;
+    }
+
+    public ArrayList<Product> getFilteredProducts(){
+
     }
 }
