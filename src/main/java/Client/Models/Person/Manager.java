2cbb5a88499d5a4cda8158387579c4c02c667f48
mmm1379 <mmmasoudpour@gmail.com>
2020-04-16 11:25:34 +0430

    add some all's to Database

--- src/main/java/Client/Models/Person/Manager.java
+++ src/main/java/Client/Models/Person/Manager.java
@@ -5,7 +5,7 @@ import Client.Models.Request;
 import java.util.ArrayList;
 
 public class Manager extends Person {
-    private static ArrayList<Request> allRequest;
+
     public ArrayList<Request> getAllRequest(){
 
     }
--- src/main/java/Server/Database.java
+++ src/main/java/Server/Database.java
@@ -1,11 +1,15 @@
 package Server;
 
 import Client.Models.Category;
+import Client.Models.Person.Person;
+import Client.Models.Request;
 
 import java.util.ArrayList;
 
 public class Database {
     private static ArrayList<Category> allCategory;
+    private static ArrayList<Request> allRequest;
+    private static ArrayList<Person> allUsers;
     //todo move all here
 
 
