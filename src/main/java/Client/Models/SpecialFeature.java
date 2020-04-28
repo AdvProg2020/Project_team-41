9cbc66d140d84c5c4df7dccdb35f3cdefc94ee42
mmm1379 <mmmasoudpour@gmail.com>
2020-04-28 18:53:47 +0430

    made special Feature a class

---
+++ src/main/java/Client/Models/SpecialFeature.java
@@ -0,0 +1,38 @@
+package Client.Models;
+
+public class SpecialFeature {
+    int specialFeatureInt;
+    String specialFeatureString;
+    public String StringOrInt(){
+        if(specialFeatureString == null)
+            return "String";
+        else
+            return "int";
+
+    }
+
+    public SpecialFeature(int specialFeatureInt) {
+        this.specialFeatureInt = specialFeatureInt;
+    }
+
+    public SpecialFeature(String specialFeatureString) {
+        this.specialFeatureString = specialFeatureString;
+    }
+
+    public int getSpecialFeatureInt() {
+        return specialFeatureInt;
+    }
+
+    public String getSpecialFeatureString() {
+        return specialFeatureString;
+    }
+
+    @Override
+    public String toString() {
+        if(StringOrInt().equals("String")){
+            return specialFeatureString;
+        }
+        else
+            return Integer.toString(specialFeatureInt);
+    }
+}
