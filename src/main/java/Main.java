7910aa57a559b518dcb2bf597f469395e98764ca
mmm1379 <mmmasoudpour@gmail.com>
2020-05-19 00:48:10 +0430

    added music to background

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,7 @@ import Client.View.Menus.MainMenu;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-
+            new SimpleAudioPlayer().playMusic();
             StartProgram.startProgram();
             MainMenu.getInstance().show();
             MainMenu.getInstance().execute();
