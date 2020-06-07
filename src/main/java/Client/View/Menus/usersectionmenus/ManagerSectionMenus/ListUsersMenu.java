e11506f5ca273f3788d108a5b3b2c0200465eae1
mmm1379 <mmmasoudpour@gmail.com>
2020-06-07 22:58:27 +0430

    designed ListUsersMenu

--- src/main/java/Client/View/Menus/usersectionmenus/ManagerSectionMenus/ListUsersMenu.java
+++ src/main/java/Client/View/Menus/usersectionmenus/ManagerSectionMenus/ListUsersMenu.java
@@ -3,58 +3,45 @@ package Client.View.Menus.UserSectionMenus.ManagerSectionMenus;
 import Client.Controller.UserSectionController.ManagerController;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.SplitMenuButton;
+import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
 import org.example.App;
 
 import java.io.IOException;
 
 public class ListUsersMenu {
-
-    public Text informationText;
     public VBox usersVBox;
-    public SplitMenuButton userButton;
+    public ScrollPane listUsersScrollPane;
 
     @FXML
     public void initialize(){
+        listUsersScrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        listUsersScrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
         for (String user : ManagerController.getInstance().getAllUsers()) {
-            usersVBox.getChildren().add(makeUserSplitButton(user));
+                makeUserSplitButton(user);
         }
     }
 
     public void createManagerProfileClicked(MouseEvent mouseEvent) {
 
     }
-    private SplitMenuButton makeUserSplitButton(String username){
-        SplitMenuButton splitMenuButton = new SplitMenuButton();
-
-        splitMenuButton.setText(username);
-        MenuItem choice1 = new MenuItem("view");
-        MenuItem choice2 = new MenuItem("remove");
-        choice1.setOnAction((e)-> {
-            removeUserClicked(choice1.getParentMenu().getText());
-        });
-        choice2.setOnAction((e)-> {
-            viewUserClicked(choice2.getParentMenu().getText());
-        });
-        splitMenuButton.getItems().addAll(choice1, choice2);
+    private void makeUserSplitButton(String username) {
         Parent root = null;
-            try {
-                root = App.loadFXML("userSection/managerSection/userSplitButton");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        try {
+            root = App.loadFXML("userSection/userSplitButton");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        AnchorPane anchorPane = (AnchorPane) root;
+        VBox vBox = (VBox) anchorPane.getChildren().get(0);
+        GridPane gridPane = (GridPane) vBox.getChildren().get(0);
+        TextField textField = (TextField) gridPane.getChildren().get(0);
+        textField.setText(username);
 
-            usersVBox.getChildren().add(root);
-        return splitMenuButton;
+        usersVBox.getChildren().add(root);
     }
-    private void removeUserClicked(String username){
 
-    }
-    private void viewUserClicked(String username){
-
-    }
 }
---
+++ src/main/java/Client/View/Menus/usersectionmenus/ManagerSectionMenus/userButtonController.java
@@ -0,0 +1,30 @@
+package Client.View.Menus.UserSectionMenus.ManagerSectionMenus;
+
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import org.example.App;
+
+import java.io.IOException;
+
+public class userButtonController {
+
+    public AnchorPane userInformation;
+    public AnchorPane usernameAnchorPane;
+    public Button removeUserButton;
+    public Button viewUserButton;
+    public GridPane gridPane;
+
+    public void viewUserClicked(MouseEvent mouseEvent) throws IOException {
+        if (gridPane == null) {
+            System.out.println("hi");
+        }
+        GridPane gridPane = (GridPane) viewUserButton.getParent().getParent().getParent();
+        gridPane.getChildren().add(App.loadFXML("userSection/view user information"));
+    }
+
+    public void removeUserClicked(MouseEvent mouseEvent) {
+
+    }
+}
