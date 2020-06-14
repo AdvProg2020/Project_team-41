0f2236283446cc6c7e97253323da7cfba4f558d7
mmm1379 <mmmasoudpour@gmail.com>
2020-06-15 00:43:51 +0430

    fixed usersection back

--- src/main/java/Client/View/Menus/gMainMenu.java
+++ src/main/java/Client/View/Menus/gMainMenu.java
@@ -5,6 +5,9 @@ import Client.Controller.UserSectionController.UserSectionController;
 import Client.Models.Person.Buyer;
 import Client.Models.Person.Manager;
 import Client.Models.Person.Seller;
+import Client.View.Menus.UserSectionMenus.BuyerSectionMenus.BuyerSectionMenu;
+import Client.View.Menus.UserSectionMenus.ManagerSectionMenus.ManagerSectionMenu;
+import Client.View.Menus.UserSectionMenus.SellerSectionMenu.SellerSectionMenu;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -54,11 +57,14 @@ public class gMainMenu extends Menu {
             userSectionError.setVisible(true);
         }else{
             if(UserSectionController.getLoggedInPerson() instanceof Manager){
+                ManagerSectionMenu.parentFxmlAddress = "mainMenu";
                 App.setRoot("userSection/managerSection/manager section");
             }else if(UserSectionController.getLoggedInPerson() instanceof Seller){
+                SellerSectionMenu.parentFxmlAddress = "mainMenu";
                 App.setRoot("userSection/sellerSection/seller section");
 
             }else if(UserSectionController.getLoggedInPerson() instanceof Buyer){
+                BuyerSectionMenu.parentFxmlAddress = "mainMenu";
                 App.setRoot("userSection/buyerSection/buyer section");
 
 
