f0f8ae38af62cdd44672e0c85bf41d80db824fde
mmm1379 <mmmasoudpour@gmail.com>
2020-06-24 10:44:53 +0430

    changed database read and write data from file(now database will be copied to folder completely)

--- src/main/java/Client/View/Menus/usersectionmenus/UserSectionMenu.java
+++ src/main/java/Client/View/Menus/usersectionmenus/UserSectionMenu.java
@@ -5,6 +5,10 @@ import Client.View.Menus.MessageType;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.CycleMethod;
+import javafx.scene.paint.LinearGradient;
+import javafx.scene.paint.Stop;
 import javafx.scene.text.Text;
 
 import java.util.ArrayList;
@@ -18,28 +22,22 @@ public class UserSectionMenu {
     public TextField emailTextField;
     public TextField phoneNumberTextField;
     public Text informationText;
-    private String previousFirstNameTextField;
-    private String previousLastNameTextField;
-    private String previousEmailTextField;
-    private String previousPhoneNumberTextField;
-
 
     @FXML
     public void initialize(){
         ArrayList<String> personalInfo = UserSectionController.getPersonalInfo(UserSectionController.getLoggedInPerson());
-        String username = personalInfo.get(0).split(" ",2)[1];
-        String firstName = personalInfo.get(1).split(" ",2)[1];
-        String lastName = personalInfo.get(2).split(" ",2)[1];
-        String credit = personalInfo.get(3).split(" ",2)[1];
-        String email = personalInfo.get(4).split(" ",2)[1];
-        String phoneNumber = personalInfo.get(5).split(" ",2)[1];
+        String username = personalInfo.get(0).split(" ")[1];
+        String firstName = personalInfo.get(1).split(" ")[1];
+        String lastName = personalInfo.get(2).split(" ")[1];
+        String credit = personalInfo.get(3).split(" ")[1];
+        String email = personalInfo.get(4).split(" ")[1];
+        String phoneNumber = personalInfo.get(5).split(" ")[1];
         userNameTextField.setText(username);
         firstNameTextField.setText(firstName);
         lastNameTextField.setText(lastName);
         creditTextField.setText(credit);
         emailTextField.setText(email);
         phoneNumberTextField.setText(phoneNumber);
-        assignPreviousTextFields();
     }
 
     public void editButtonClicked(MouseEvent mouseEvent) {
@@ -49,10 +47,8 @@ public class UserSectionMenu {
         int numberOfEdits = 0;
         int numberOfErrors = 0;
         try {
-            if (!previousFirstNameTextField.equals(edit)) {
-                UserSectionController.edit("firstName", edit);
-                numberOfEdits++;
-            }
+            UserSectionController.edit("firstName", edit);
+            numberOfEdits++;
         } catch (Exception e) {
             errors.append(e.getMessage()).append("-");
             numberOfErrors++;
@@ -60,10 +56,8 @@ public class UserSectionMenu {
         }
         edit = lastNameTextField.getText();
         try {
-            if (!previousLastNameTextField.equals(edit)) {
-                UserSectionController.edit("lastName", edit);
-                numberOfEdits++;
-            }
+            UserSectionController.edit("lastName", edit);
+            numberOfEdits++;
         } catch (Exception e) {
             errors.append(e.getMessage()).append("-");
             numberOfErrors++;
@@ -71,10 +65,8 @@ public class UserSectionMenu {
         }
         edit = emailTextField.getText();
         try {
-            if (!previousEmailTextField.equals(edit)) {
-                UserSectionController.edit("email", edit);
-                numberOfEdits++;
-            }
+            UserSectionController.edit("email", edit);
+            numberOfEdits++;
         } catch (Exception e) {
             errors.append(e.getMessage()).append("-");
             numberOfErrors++;
@@ -82,10 +74,8 @@ public class UserSectionMenu {
         }
         edit = phoneNumberTextField.getText();
         try {
-            if (!previousPhoneNumberTextField.equals(edit)) {
-                UserSectionController.edit("phoneNumber", edit);
-                numberOfEdits++;
-            }
+            UserSectionController.edit("phoneNumber", edit);
+            numberOfEdits++;
         } catch (Exception e) {
             errors.append(e.getMessage()).append("-");
             numberOfErrors++;
@@ -97,22 +87,12 @@ public class UserSectionMenu {
         else{
             showMessage(MessageType.ERROR, errors.substring(0, errors.length() - 1));
         }
-        if (numberOfEdits == 0) {
-            showMessage(MessageType.INFORMATION,"no edits detected");
-        }
-        assignPreviousTextFields();
 
     }
     private void showMessage(MessageType messageType,String message){
         informationText.setFill(messageType.getLinearGradient());
         informationText.setText(message);
 
-    }
-    private void assignPreviousTextFields(){
-                previousFirstNameTextField = firstNameTextField.getText();
-                previousLastNameTextField = lastNameTextField.getText();
-        previousEmailTextField = emailTextField.getText();
-                previousPhoneNumberTextField = phoneNumberTextField.getText();
     }
 
 }
