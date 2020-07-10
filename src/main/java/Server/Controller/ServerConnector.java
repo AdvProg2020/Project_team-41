538125d6f2607143bb6ece45bd2fa03a199fbbc3
mmm1379 <mmmasoudpour@gmail.com>
2020-07-10 23:58:38 +0430

    separated client userSections from server

--- src/main/java/Server/Controller/ServerConnector.java
+++ src/main/java/Server/Controller/ServerConnector.java
@@ -2,8 +2,14 @@ package Server.Controller;
 
 import Client.Models.Message.Message;
 import Client.Models.Message.MessageType;
+import Client.Models.Person.Buyer;
 import Client.Models.Person.Person;
+import Client.Models.Person.Seller;
+import Client.Models.Score;
+import Server.Controller.UserSectionController.BuyerServerController;
 import Server.Controller.UserSectionController.ManagerServerController;
+import Server.Controller.UserSectionController.SellerServerController;
+import Server.Controller.UserSectionController.UserSectionServerController;
 
 import java.io.IOException;
 
@@ -11,6 +17,8 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class ServerConnector implements Runnable {
     private static ServerSocket serverSocket;
@@ -55,6 +63,10 @@ public class ServerConnector implements Runnable {
     private void processMessage(Message message) throws Exception {
         MessageType.ClassTypes classTypes = message.getMessageType().getClassTypes();
         switch (classTypes) {
+            case USER_SECTION:{
+                processUserMessage(message);
+                break;
+            }
             case MANAGER_SECTION: {
                 processManagerMessage(message);
                 break;
@@ -65,6 +77,19 @@ public class ServerConnector implements Runnable {
             }
             case BUYER_SECTION:{
                 processBuyerMessage(message);
+                break;
+            }
+        }
+    }
+    private void processUserMessage(Message message) throws Exception{
+        Object[] inputs = message.getInputs();
+        switch (message.getMessageType()) {
+            case GET_PERSONAL_INFO:{
+                UserSectionServerController.getPersonalInfo((Person) inputs[0]);
+                break;
+            }
+            case EDIT:{
+                UserSectionServerController.edit((Person) inputs[0], (String) inputs[1], (String) inputs[2]);
                 break;
             }
         }
@@ -90,19 +115,227 @@ public class ServerConnector implements Runnable {
                 objectOutputStream.writeObject(ManagerServerController.getInstance().getAllUsers());
                 break;
             }
+            case GET_USER_BY_USERNAME:{
+                objectOutputStream.writeObject(ManagerServerController.getInstance().getUserByUsername((String) inputs[0]));
+                break;
+            }
+            case DELETE_USER:{
+                ManagerServerController.getInstance().deleteUser((String) inputs[0]);
+                sendSuccessful();
+                break;
+            }
+            case CREATE_MANAGER_PROFILE:{
+                ManagerServerController.getInstance().createManagerProfile((ArrayList<String>) inputs[0]);
+                sendSuccessful();
+                break;
+            }
+            case REMOVE_PRODUCT_MANAGER_SECTION:{
+                ManagerServerController.getInstance().removeProduct((String) inputs[0]);
+                sendSuccessful();
+                break;
+            }
+            case CREATE_DISCOUNT_CODE:{
+                ManagerServerController.getInstance().createDiscountCode((ArrayList<String>) inputs[0]);
+                sendSuccessful();
+                break;
+            }
+            case VIEW_DISCOUNT_CODE:{
+                objectOutputStream.writeObject(ManagerServerController.getInstance().viewDiscountCode((String)inputs[0]));
+                break;
+            }
+            case EDIT_DISCOUNT_CODE:{
+                ManagerServerController.getInstance().editDiscountCode((String)inputs[0], (HashMap<String, String>) inputs[1]);
+                sendSuccessful();
+                break;
+            }
+            case REMOVE_DISCOUNT_CODE:{
+                ManagerServerController.getInstance().removeDiscountCode((String) inputs[0]);
+                sendSuccessful();
+                break;
+            }
+            case SHOW_REQUEST:{
+                objectOutputStream.writeObject(ManagerServerController.getInstance().showRequest());
+                break;
+            }
+            case GET_REQUEST_DETAILS:{
+                objectOutputStream.writeObject(ManagerServerController.getInstance().getRequestDetails((String) inputs[0]));
+                break;
+            }
+            case ACCEPT_REQUEST:{
+                ManagerServerController.getInstance().acceptRequest((String) inputs[0]);
+                sendSuccessful();
+                break;
+            }
+            case DECLINE_REQUEST:{
+                ManagerServerController.getInstance().declineRequest((String) inputs[0]);
+                sendSuccessful();
+                break;
+            }
+            case SHOW_CATEGORIES:{
+                objectOutputStream.writeObject(ManagerServerController.getInstance().showCategories());
+                break;
+            }
+            case EDIT_CATEGORY_NAME:{
+                ManagerServerController.getInstance().editCategoryName((String)inputs[0], (String) inputs[1]);
+                sendSuccessful();
+                break;
+            }
+            case ADD_CATEGORY:{
+                ManagerServerController.getInstance().addCategory((String)inputs[0], (String) inputs[1]);
+                sendSuccessful();
+                break;
+            }
+            case REMOVE_CATEGORY:{
+                ManagerServerController.getInstance().removeCategory((String) inputs[0]);
+                sendSuccessful();
+                break;
+            }
+            case GET_CATEGORY_SPECIAL_FEATURES_MANAGER_SECTION:{
+                objectOutputStream.writeObject(ManagerServerController.getInstance().getCategorySpecialFeatures((String) inputs[0]));
+                break;
+            }
+            case VIEW_ALL_DISCOUNT_CODES:{
+                objectOutputStream.writeObject(ManagerServerController.getInstance().viewAllDiscountCodes());
+                break;
+            }
+            case GET_PRODUCT_BY_ID:{
+                objectOutputStream.writeObject(ManagerServerController.getInstance().getProductById((String) inputs[0]));
+                break;
+            }
+
         }
     }
     private void processSellerMessage(Message message) throws Exception {
-
+        Object[] inputs = message.getInputs();
+        switch (message.getMessageType()) {
+            case GET_CATEGORY_SPECIAL_FEATURES_SELLER_SECTION:{
+                objectOutputStream.writeObject(SellerServerController.getInstance().getCategorySpecialFeatures((String) inputs[0]));
+                break;
+            }
+            case GET_PRODUCT_BUYERS:{
+                objectOutputStream.writeObject(SellerServerController.getInstance().getProductBuyers((String) inputs[0]));
+                break;
+            }
+            case GET_FACTORY_NAME:{
+                objectOutputStream.writeObject(SellerServerController.getInstance().getFactoryName((Seller) inputs[0]));
+                break;
+            }
+            case GET_SALES_HISTORY:{
+                objectOutputStream.writeObject(SellerServerController.getInstance().getSalesHistory((Seller) inputs[0]));
+                break;
+            }
+            case GET_LOGS:{
+                objectOutputStream.writeObject(SellerServerController.getInstance().getLogs((Seller) inputs[0]));
+                break;
+            }
+            case GET_PRODUCTS:{
+                objectOutputStream.writeObject(SellerServerController.getInstance().getProducts((Seller) inputs[0]));
+                break;
+            }
+            case GET_PRODUCT_SELLER_SECTION:{
+                objectOutputStream.writeObject(SellerServerController.getInstance().getProduct((Seller)inputs[0], (String) inputs[1]));
+                break;
+            }
+            case GET_BUYERS:{
+                objectOutputStream.writeObject(SellerServerController.getInstance().getBuyers((Seller)inputs[0], (String) inputs[1]));
+                break;
+            }
+            case EDIT_PRODUCT:{
+                SellerServerController.getInstance().editProduct((Seller)inputs[0],(String)inputs[1], (HashMap<String, String>) inputs[2]);
+                sendSuccessful();
+                break;
+            }
+            case ADD_PRODUCT:{
+                SellerServerController.getInstance().addProduct((Seller) inputs[0], (ArrayList<String>) inputs[1]);
+                sendSuccessful();
+                break;
+            }
+            case REMOVE_PRODUCT_SELLER_SECTION:{
+                SellerServerController.getInstance().removeProduct((Seller)inputs[0], (String) inputs[1]);
+                sendSuccessful();
+                break;
+            }
+            case GET_CATEGORIES:{
+                objectOutputStream.writeObject(SellerServerController.getInstance().getCategories());
+                break;
+            }
+            case GET_OFFS:{
+                objectOutputStream.writeObject(SellerServerController.getInstance().getOffs((Seller)inputs[0]));
+                break;
+            }
+            case GET_OFF:{
+                objectOutputStream.writeObject(SellerServerController.getInstance().getOff((Seller)inputs[0], (String) inputs[1]));
+                break;
+            }
+            case EDIT_OFF:{
+                SellerServerController.getInstance().editOff((String)inputs[0],(Seller)inputs[1], (HashMap<String, String>) inputs[2]);
+                sendSuccessful();
+                break;
+            }
+            case ADD_OFF:{
+                SellerServerController.getInstance().addOff((Seller)inputs[0], (ArrayList<String>) inputs[1]);
+                sendSuccessful();
+                break;
+            }
+        }
     }
     private void processBuyerMessage(Message message) throws Exception {
+        Object[] inputs = message.getInputs();
+        switch (message.getMessageType()) {
+            case GET_CODED_DISCOUNTS:{
+                objectOutputStream.writeObject(BuyerServerController.getInstance().getCodedDiscounts((Person) inputs[0]));
+                break;
+            }
+            case PAY_FOR_THE_SHOP:{
+                BuyerServerController.payForTheShop((Buyer) inputs[0]);
+                sendSuccessful();
+                break;
+            }
+            case ADD_CODED_DISCOUNT_TO_CART:{
+                BuyerServerController.getInstance().addCodedDiscountToCart((Buyer)inputs[0], (String) inputs[1]);
+                sendSuccessful();
 
+                break;
+            }
+            case REMOVE_CODED_DISCOUNT_FROM_CART:{
+                BuyerServerController.getInstance().removeCodedDiscountFromCart((Buyer) inputs[0]);
+                sendSuccessful();
+
+                break;
+            }
+            case RATE_THE_PRODUCT:{
+                BuyerServerController.getInstance().rateTheProduct((Buyer)inputs[0],(String)inputs[1], (Score) inputs[2]);
+                sendSuccessful();
+
+                break;
+            }
+            case GET_PRODUCT_BUYER_SECTION:{
+                objectOutputStream.writeObject(BuyerServerController.getInstance().getProduct((String) inputs[0]));
+                break;
+            }
+            case GET_CODED_DISCOUNT:{
+                objectOutputStream.writeObject(BuyerServerController.getInstance().getCodedDiscount((String) inputs[0], (Person) inputs[1]));
+                break;
+            }
+            case INCREASE_PRODUCT:{
+                BuyerServerController.getInstance().increaseProduct((Buyer)inputs[0],(Integer)inputs[1], (String) inputs[2]);
+                sendSuccessful();
+
+                break;
+            }
+            case DECREASE_PRODUCT:{
+                BuyerServerController.getInstance().decreaseProduct((Buyer)inputs[0],(Integer)inputs[1], (String) inputs[2]);
+                sendSuccessful();
+
+                break;
+            }
+        }
     }
 
     //send after every void method
     private void sendSuccessful(){
         try {
-            objectOutputStream.writeObject(new Message(MessageType.SUCCESSFULL));
+            objectOutputStream.writeObject(new Message(MessageType.SUCCESSFUL));
         } catch (IOException e) {
             e.printStackTrace();
         }
--- src/main/java/client/Controller/UserSectionController/ManagerController.java
+++ src/main/java/client/Controller/UserSectionController/ManagerController.java
@@ -2,7 +2,7 @@ package Client.Controller.UserSectionController;
 
 import Client.Controller.Connector;
 import Client.Models.Message.Message;
-import Client.Models.Message.MessageTypes;
+import Client.Models.Message.MessageType;
 import Client.Models.Person.Person;
 import Client.Models.Product;
 import Server.Controller.UserSectionController.ManagerServerController;
@@ -19,83 +19,102 @@ public class ManagerController extends UserSectionController{
 
         return single_instance;
     }
-    public void acceptAllRequests() throws Exception {
-        Connector.getInstance().initializeMessage(new Message(null, MessageTypes.ACCEPT_ALL_REQUESTS));
-        ManagerServerController.getInstance().acceptAllRequests();
-    }
+
     private ManagerController(){
     }
-    public void editCategorySpecialFeatures(String category,String editedField) throws Exception {
-        Message message = new Message(new Object[]{category, editedField}, MessageTypes.EDIT_CATEGORY_SPECIAL_FEATURES);
-        Connector.getInstance().sendMessage(message);
-        Connector.getInstance().receiveMessage();
+    public void acceptAllRequests() throws Exception {
+        Connector.getInstance().initializeMessage(new Message(null, MessageType.ACCEPT_ALL_REQUESTS));
 
-        ManagerServerController.getInstance().editCategorySpecialFeatures(category,editedField);
+        // ManagerServerController.getInstance().acceptAllRequests();
+    }
+    public void editCategorySpecialFeatures(String category,String editedField) throws Exception {
+        Connector.getInstance().initializeMessage(new Message(new Object[]{category, editedField}, MessageType.EDIT_CATEGORY_SPECIAL_FEATURES));
+
+        // ManagerServerController.getInstance().editCategorySpecialFeatures(category,editedField);
     }
     public ArrayList<Product> getAllProducts() throws Exception {
-        Message message = new Message(null, MessageTypes.GET_ALL_PRODUCTS);
-        Connector.getInstance().sendMessage(message);
-        return (ArrayList<Product>) Connector.getInstance().receiveMessage();
+        return (ArrayList<Product>) Connector.getInstance().initializeMessage(new Message(null, MessageType.GET_ALL_PRODUCTS));
+
         //return ManagerServerController.getInstance().getAllProducts();
     }
-    public ArrayList<String> getAllUsers(){
-        return ManagerServerController.getInstance().getAllUsers();
+    public ArrayList<String> getAllUsers() throws Exception {
+        return (ArrayList<String>) Connector.getInstance().initializeMessage(new Message(null, MessageType.GET_ALL_USERS));
+//        return ManagerServerController.getInstance().getAllUsers();
     }
     public Person getUserByUsername(String username) throws Exception {
-        return ManagerServerController.getInstance().getUserByUsername(username);
+        return (Person) Connector.getInstance().initializeMessage(new Message(new Object[]{username}, MessageType.GET_USER_BY_USERNAME));
+        //return ManagerServerController.getInstance().getUserByUsername(username);
     }
     public void deleteUser(String username) throws Exception {
-        ManagerServerController.getInstance().deleteUser(username);
+        Connector.getInstance().initializeMessage(new Message(new Object[]{username}, MessageType.DELETE_USER));
+        //ManagerServerController.getInstance().deleteUser(username);
     }
     public void createManagerProfile(ArrayList<String> userInfo) throws Exception {
-        ManagerServerController.getInstance().createManagerProfile(userInfo);
+        Connector.getInstance().initializeMessage(new Message(new Object[]{userInfo}, MessageType.CREATE_MANAGER_PROFILE));
+        //ManagerServerController.getInstance().createManagerProfile(userInfo);
     }
     public void removeProduct(String productId) throws Exception {
-        ManagerServerController.getInstance().removeProduct(productId);
+        Connector.getInstance().initializeMessage(new Message(new Object[]{productId}, MessageType.REMOVE_PRODUCT_MANAGER_SECTION));
+//        ManagerServerController.getInstance().removeProduct(productId);
     }
     public void createDiscountCode(ArrayList<String> codeInformation) throws Exception {
-        ManagerServerController.getInstance().createDiscountCode(codeInformation);
+        Connector.getInstance().initializeMessage(new Message(new Object[]{codeInformation}, MessageType.CREATE_DISCOUNT_CODE));
+//        ManagerServerController.getInstance().createDiscountCode(codeInformation);
     }
     public ArrayList<String> viewDiscountCode(String code) throws Exception {
-        return ManagerServerController.getInstance().viewDiscountCode(code);
+        return (ArrayList<String>) Connector.getInstance().initializeMessage(new Message(new Object[]{code}, MessageType.VIEW_DISCOUNT_CODE));
+//        return ManagerServerController.getInstance().viewDiscountCode(code);
     }
     public void  editDiscountCode(String code,HashMap<String,String> edits) throws Exception {
-        ManagerServerController.getInstance().editDiscountCode(code,edits);
+        Connector.getInstance().initializeMessage(new Message(new Object[]{code,edits}, MessageType.EDIT_DISCOUNT_CODE));
+//        ManagerServerController.getInstance().editDiscountCode(code,edits);
     }
     public void removeDiscountCode(String code) throws Exception {
-        ManagerServerController.getInstance().removeDiscountCode(code);
+        Connector.getInstance().initializeMessage(new Message(new Object[]{code}, MessageType.REMOVE_DISCOUNT_CODE));
+//        ManagerServerController.getInstance().removeDiscountCode(code);
     }
-    public ArrayList<String> showRequest(){
-        return ManagerServerController.getInstance().showRequest();
+    public ArrayList<String> showRequest() throws Exception{
+        return (ArrayList<String>) Connector.getInstance().initializeMessage(new Message(null, MessageType.SHOW_REQUEST));
+//        return ManagerServerController.getInstance().showRequest();
     }
     public ArrayList<String> getRequestDetails(String request) throws Exception {
-        return ManagerServerController.getInstance().getRequestDetails(request);
+        return (ArrayList<String>) Connector.getInstance().initializeMessage(new Message(new Object[]{request}, MessageType.GET_REQUEST_DETAILS));
+//        return ManagerServerController.getInstance().getRequestDetails(request);
     }
     public void acceptRequest(String request) throws Exception {
-        ManagerServerController.getInstance().acceptRequest(request);
+        Connector.getInstance().initializeMessage(new Message(new Object[]{request}, MessageType.ACCEPT_REQUEST));
+//        ManagerServerController.getInstance().acceptRequest(request);
     }
     public void declineRequest(String request) throws Exception {
-        ManagerServerController.getInstance().declineRequest(request);
+        Connector.getInstance().initializeMessage(new Message(new Object[]{request}, MessageType.DECLINE_REQUEST));
+//        ManagerServerController.getInstance().declineRequest(request);
     }
-    public ArrayList<String> showCategories(){
-        return ManagerServerController.getInstance().showCategories();
+    public ArrayList<String> showCategories() throws Exception{
+        return (ArrayList<String>) Connector.getInstance().initializeMessage(new Message(null, MessageType.SHOW_CATEGORIES));
+//        return ManagerServerController.getInstance().showCategories();
     }
     public void editCategoryName(String category, String editedField) throws Exception {
-        ManagerServerController.getInstance().editCategoryName(category, editedField);
+        Connector.getInstance().initializeMessage(new Message(new Object[]{category,editedField}, MessageType.EDIT_CATEGORY_NAME));
+//        ManagerServerController.getInstance().editCategoryName(category, editedField);
     }
     public void addCategory(String categoryName,String specialFeatures) throws Exception {
-        ManagerServerController.getInstance().addCategory(categoryName,specialFeatures);
+        Connector.getInstance().initializeMessage(new Message(new Object[]{categoryName,specialFeatures}, MessageType.ADD_CATEGORY));
+//        ManagerServerController.getInstance().addCategory(categoryName,specialFeatures);
     }
     public void removeCategory(String categoryName) throws Exception {
-        ManagerServerController.getInstance().removeCategory(categoryName);
+        Connector.getInstance().initializeMessage(new Message(new Object[]{categoryName}, MessageType.REMOVE_CATEGORY));
+//        ManagerServerController.getInstance().removeCategory(categoryName);
     }
     public ArrayList<String> getCategorySpecialFeatures(String categoryName) throws Exception {
-        return ManagerServerController.getInstance().getCategorySpecialFeatures(categoryName);
+        return (ArrayList<String>) Connector.getInstance().initializeMessage(new Message(new Object[]{categoryName}, MessageType.GET_CATEGORY_SPECIAL_FEATURES_MANAGER_SECTION));
+//        return ManagerServerController.getInstance().getCategorySpecialFeatures(categoryName);
     }
-    public ArrayList<String> viewAllDiscountCodes(){
-        return ManagerServerController.getInstance().viewAllDiscountCodes();
+    public ArrayList<String> viewAllDiscountCodes() throws Exception{
+        return (ArrayList<String>) Connector.getInstance().initializeMessage(new Message(null, MessageType.VIEW_ALL_DISCOUNT_CODES));
+//        return ManagerServerController.getInstance().viewAllDiscountCodes();
     }
     public Product getProductById(String productId) throws Exception {
-        return ManagerServerController.getInstance().getProductById(productId);
+        return (Product) Connector.getInstance().initializeMessage(new Message(new Object[]{productId}, MessageType.GET_PRODUCT_BY_ID));
+//        return ManagerServerController.getInstance().getProductById(productId);
     }
 }
