31e3035aed7628526e4ea524a83eb652564736cb
mmm1379 <mmmasoudpour@gmail.com>
2020-07-11 01:36:14 +0430

    fixed objectOutput blocking problems

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -25,7 +25,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         Socket socket = new Socket("localhost", 8888);
-        Connector.makeInstance(socket);
+        new Thread(() -> Connector.makeInstance(socket));
         SimpleAudioPlayer.getInstance().playMusic(Music.MAIN_MENU);
         setIconImage();
         StartProgram.startProgram();
