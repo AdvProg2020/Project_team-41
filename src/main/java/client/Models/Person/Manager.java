440828dfa3ed022ed0a8fe46c02c7c6a004d323d
mmm1379 <mmmasoudpour@gmail.com>
2020-07-12 11:29:00 +0430

    deleted UserSectionMenu.java

--- src/main/java/client/Models/Person/Manager.java
+++ src/main/java/client/Models/Person/Manager.java
@@ -1,5 +1,16 @@
 package Client.Models.Person;
 
-public class Manager extends Person {
+import Client.Controller.bankController.BankAPI;
 
+
+public class Manager extends Person {
+    private static int accountId;
+    public void setUpManagerAccountId(Manager manager,String username,String password) throws Exception {
+        BankAPI.makeInstance();
+        accountId = BankAPI.getInstance().createAccount(manager.getFirstName(), manager.getLastName(), username, password, password);
+    }
+
+    public static int getAccountId() {
+        return accountId;
+    }
 }
