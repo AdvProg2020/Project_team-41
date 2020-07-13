baf9a8ce35a30d8755ef0d391b551cc00e57f249
mahdimostajer <mahdi.mostajeran.ce@gmail.com>
2020-07-13 19:30:11 +0430

    chat isn't working

--- src/main/java/Client/View/Menus/BuyerBackup/bNameOfUser.java
+++ src/main/java/Client/View/Menus/BuyerBackup/bNameOfUser.java
@@ -8,7 +8,9 @@ import Client.Models.Message.Message;
 import Client.Models.Message.MessageType;
 import Client.Models.Person.BackupPerson;
 import Client.View.Menus.NodeFinder;
+import Client.View.Menus.UserSectionMenus.BackupPersonMenu.Comment;
 import Client.View.Menus.UserSectionMenus.UserSection;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
@@ -18,17 +20,24 @@ import org.example.App;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class bNameOfUser {
     public static String user;
     public Label username;
     public AnchorPane userAnchorPane;
+    public static AnchorPane fuckAnchorPane;
+    public static String currentUSer;
+    public static boolean hasHeChoose;
 
     public void initialize(){
         username.setText(user);
     }
 
     public void openChat(ActionEvent touchEvent) throws IOException {
+        fuckAnchorPane=userAnchorPane;
+        currentUSer=username.getText();
         BorderPane borderPane = (BorderPane) NodeFinder.getParentById(userAnchorPane, "mainBorderPane");
         AnchorPane nameAnchorPane = (AnchorPane) NodeFinder.getChildById(borderPane, "nameAnchorPane");
         nameAnchorPane.getChildren().clear();
@@ -40,22 +49,55 @@ public class bNameOfUser {
         sendAnchorPane.getChildren().add(App.loadFXML("buyerBackUp/sendBox"));
 
 
-        ChatBox chatBox = null;
-        try {
-            chatBox = (ChatBox) Connector.getInstance().initializeMessage(new Message(new Object[]{username.getText(),
-                    UserSectionController.getLoggedInPerson().getUserName()}, MessageType.GET_CHAT_BOX));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
 
-        VBox vBox = (VBox) NodeFinder.getChildById(borderPane, "chatVBox");
-        vBox.getChildren().clear();
-        for (ChatComment chatComment : chatBox.getChatComments()) {
-            bComment.chatComment = chatComment;
-            vBox.getChildren().add(App.loadFXML("buyerBackUp/comment"));
-
+        if(hasHeChoose==false) {
+            hasHeChoose=true;
+            updateComments();
         }
 
 
+    }
+
+    public static void updateComments(){
+        Timer animTimer = new Timer();
+        animTimer.scheduleAtFixedRate(new TimerTask() {
+
+            @Override
+            public void run() {
+                if (currentUSer!=null) {
+                    ChatBox chatBox = null;
+                    try {
+                        chatBox = (ChatBox) Connector.getInstance().initializeMessage(new Message(new Object[]{currentUSer,
+                                UserSectionController.getLoggedInPerson().getUserName()}, MessageType.GET_CHAT_BOX));
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                    putComments(chatBox);
+
+                } else {
+                    this.cancel();
+                }
+
+            }
+
+        }, 0, 1000);
+    }
+    public static void putComments(ChatBox chatBox){
+
+        Platform.runLater(() -> {
+            BorderPane borderPane = (BorderPane) NodeFinder.getParentById(fuckAnchorPane, "mainBorderPane");
+            VBox vBox = (VBox) NodeFinder.getChildById(borderPane, "chatVBox");
+            vBox.getChildren().clear();
+            for (ChatComment chatComment : chatBox.getChatComments()) {
+                bComment.chatComment = chatComment;
+                try {
+                    vBox.getChildren().add(App.loadFXML("buyerBackUp/comment"));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
     }
 }
--- src/main/java/Client/View/Menus/UserSectionMenus/BackupPersonMenu/NameOfUser.java
+++ src/main/java/Client/View/Menus/UserSectionMenus/BackupPersonMenu/NameOfUser.java
@@ -9,6 +9,7 @@ import Client.Models.Message.MessageType;
 import Client.View.Menus.BuyerBackup.bComment;
 import Client.View.Menus.BuyerBackup.bNameBox;
 import Client.View.Menus.NodeFinder;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
@@ -17,17 +18,24 @@ import javafx.scene.layout.VBox;
 import org.example.App;
 
 import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class NameOfUser {
     public static String user;
     public Label username;
     public AnchorPane userAnchorPane;
+    public static AnchorPane fuckAnchorPane;
+    public static String currentUSer;
+    public static boolean hasHeChoose;
 
     public void initialize(){
         username.setText(user);
     }
 
     public void openChat(ActionEvent touchEvent) throws IOException {
+        fuckAnchorPane=userAnchorPane;
+        currentUSer=username.getText();
         BorderPane borderPane = (BorderPane) NodeFinder.getParentById(userAnchorPane, "mainBorderPane");
         AnchorPane nameAnchorPane = (AnchorPane) NodeFinder.getChildById(borderPane, "nameAnchorPane");
         nameAnchorPane.getChildren().clear();
@@ -37,22 +45,53 @@ public class NameOfUser {
         AnchorPane sendAnchorPane = (AnchorPane) NodeFinder.getChildById(borderPane, "sendAnchorPane");
         sendAnchorPane.getChildren().clear();
         sendAnchorPane.getChildren().add(App.loadFXML("userSection/backupPersonSection/sendBox"));
-
-
-        ChatBox chatBox = null;
-        try {
-            chatBox = (ChatBox) Connector.getInstance().initializeMessage(new Message(new Object[]{UserSectionController.getLoggedInPerson().getUserName(),
-                    username.getText()}, MessageType.GET_CHAT_BOX));
-        } catch (Exception e) {
-            e.printStackTrace();
+        if(hasHeChoose==false) {
+            hasHeChoose=true;
+            updateComments();
         }
 
-        VBox vBox = (VBox) NodeFinder.getChildById(borderPane, "chatVBox");
-        vBox.getChildren().clear();
-        for (ChatComment chatComment : chatBox.getChatComments()) {
-            Comment.chatComment = chatComment;
-            vBox.getChildren().add(App.loadFXML("userSection/backupPersonSection/Comment"));
 
-        }
+    }
+    public static void updateComments(){
+        Timer animTimer = new Timer();
+        animTimer.scheduleAtFixedRate(new TimerTask() {
+
+            @Override
+            public void run() {
+                if (currentUSer!=null) {
+                    ChatBox chatBox = null;
+                    try {
+                        chatBox = (ChatBox) Connector.getInstance().initializeMessage(new Message(new Object[]{UserSectionController.getLoggedInPerson().getUserName(),
+                                currentUSer}, MessageType.GET_CHAT_BOX));
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                        putComments(chatBox);
+
+                } else {
+                    this.cancel();
+                }
+
+            }
+
+        }, 0, 1000);
+    }
+    public static void putComments(ChatBox chatBox){
+
+        Platform.runLater(() -> {
+            BorderPane borderPane = (BorderPane) NodeFinder.getParentById(fuckAnchorPane, "mainBorderPane");
+            VBox vBox = (VBox) NodeFinder.getChildById(borderPane, "chatVBox");
+            vBox.getChildren().clear();
+            for (ChatComment chatComment : chatBox.getChatComments()) {
+                Comment.chatComment = chatComment;
+                try {
+                    vBox.getChildren().add(App.loadFXML("userSection/backupPersonSection/Comment"));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
     }
 }
