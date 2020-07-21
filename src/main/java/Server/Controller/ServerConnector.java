5c6eec7da66fd809066cf5d01c77b40b3ac8872f
mmm1379 <mmmasoudpour@gmail.com>
2020-07-21 10:49:53 +0430

    fixed printing file in console!

--- src/main/java/Server/Controller/ServerConnector.java
+++ src/main/java/Server/Controller/ServerConnector.java
@@ -59,7 +59,12 @@ public class ServerConnector extends Thread {
                     objectOutputStream.reset();
                     Message message = (Message) objectInputStream.readObject();
                     System.out.println("message received");
-                    System.out.println(message);
+                    if((message.getOutput() instanceof List))
+                        if(((List) message.getOutput()).size()>500){
+                            System.out.println("large list message!");
+                        }
+                        else
+                            System.out.println(message);
                     System.err.println("----------------------");
                     processMessage(message);
                     ServerSaver.write(AllCommands.allData);
